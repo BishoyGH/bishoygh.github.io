002bf5ae6f96d80f5b0b4bd765488e6aec5ca7a1
BishoyGH <bishoyfox15@gmail.com>
2024-07-15 01:01:52 +0300

    update hero title and subtitle for small screens

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,10 +7,10 @@ const Hero: React.FC = () => {
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
           <div className="flex flex-col items-center lg:items-start">
-            <h1 className="bg-gradient-to-r from-purple-400 via-indigo-400 to-purple-500 bg-clip-text pb-4 text-6xl font-medium tracking-tight text-transparent md:text-7xl lg:mt-16">
+            <h1 className="bg-gradient-to-r from-purple-400 via-indigo-400 to-purple-500 bg-clip-text pb-4 text-6xl font-medium tracking-tight text-transparent max-[450px]:text-4xl max-[450px]:font-black md:text-7xl lg:mt-16">
               Bishoy Gamal
             </h1>
-            <span className="bg-gradient-to-br from-neutral-300 via-neutral-400 to-neutral-500 bg-clip-text text-2xl tracking-tight text-transparent lg:ms-1">
+            <span className="bg-gradient-to-br from-neutral-300 via-neutral-400 to-neutral-500 bg-clip-text text-2xl tracking-tight text-transparent max-[450px]:text-center max-[450px]:text-xl lg:ms-1">
               Frontend Web Developer
             </span>
             <p className="my-2 max-w-xl py-6 font-light tracking-tighter">
